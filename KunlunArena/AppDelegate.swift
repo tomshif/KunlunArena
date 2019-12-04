7049acd8a0f8600c82fce5cda52f94f9dbbdcfca
Tom Shiflet <tom.shiflet@mylcs.com>
2019-12-03 19:31:53 -0600

    Initial Commit

---
+++ KunlunArena/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  KunlunArena
+//
+//  Created by Tom Shiflet on 12/3/19.
+//  Copyright © 2019 LCS Game Design. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
