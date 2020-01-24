a45968c0d8faa97f0d206d6810d73ec747c269a2
Aidan Woodworth <aidan.woodworth20@mylcs.com>
2020-01-24 15:18:16 -0600

    added sword of lightning

---
+++ KunlunArena/Core Classes/Ability Classes/SwordsOfLightningTalentClass.swift
@@ -0,0 +1,42 @@
+//
+//  SwordsOfLightningTalentClass.swift
+//  KunlunArena
+//
+//  Created by 5 - Game Design on 1/23/20.
+//  Copyright © 2020 LCS Game Design. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class SwordsOfLightningTalentClass:PlayerTalentClass
+{
+    var timeSinceLastUse: CGFloat=0
+    
+    override init(theGame:GameClass)
+    {
+        super.init(theGame: theGame)
+        name="Swords of Lighting"
+        description="increases attack speed by 100% for 5 seconds"
+        isAction=false
+        game=theGame
+        COOLDOWN=15
+        lengthActive=5
+    }//init game
+    
+    override func removeTalent()
+    {
+        isActive=false
+    }
+    
+    override func updateTalent()
+    {
+        
+    }
+    
+    override func doTalent()
+    {
+        
+    }
+    
+}
